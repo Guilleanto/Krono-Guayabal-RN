b3a1bdf182b18e947dfc59358ba2569ea66d4c21
Guillermo Ochoa <guillermoantonio8a@gmail.com>
2019-07-15 11:54:19 -0500

    testing svg

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import HeaderTest from '../header/header'
+import {View} from 'react-native'
+import SvgComponent from '../svg/guayabalIcon'
 
 export default class Home extends React.Component<any,any>{
     constructor(props:any){
@@ -7,7 +9,11 @@ export default class Home extends React.Component<any,any>{
     }
     render(){
         return(
-            <HeaderTest/>
+            <View>
+                <HeaderTest/>
+                <SvgComponent></SvgComponent>
+            </View>
+            
         )
     }
 }
